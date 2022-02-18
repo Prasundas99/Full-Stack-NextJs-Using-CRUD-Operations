e2e209b5d4e86efacc635b72c2622972e2e2996c
Prasundas99 <prasundas115@gmail.com>
2022-02-18 23:25:01 +0530

    feat:db connected in next

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    reactStrictMode: true,
+    env:{
+        MONGO_URI: " mongodb+srv://user1:user1@cluster0.nn5xt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    }
+}
